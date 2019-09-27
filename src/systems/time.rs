31a9db6a1b5eb414334be379ad52d24cc1681c23
Ian Sly <iansly@protonmail.com>
2019-09-27 15:03:17 -0700

    finishing touches

--- src/systems/time.rs
+++ src/systems/time.rs
@@ -1,87 +1,26 @@
 use specs::prelude::*;
 use crate::BASE_TURN_TIME;
-<<<<<<< HEAD
-// use shrev::{EventChannel, ReaderId};
-use crate::components::{Actor, MyTurn, PlayerControl};
-use crate::components::flags::ActionResult;
-
-// turn structure
-// --> TurnAllocator       | finds actors who have 0 fatigue and no MyTurn component, and gives them a MyTurn.
-// --> Fatigue             | decrements every actor's fatigue clamped to 0.
-// --> StartTurn           | 
-// --> PlayerStartTurn     | checks if player has MyTurn and pauses game.
-// --> Action              | gets actor actions and removes MyTurn
-    // --> Movement, attack, etc... | responsible for actors actually doing stuff on their own accord and generating ActionResults
-
-// --> EndTurn             | iterates through ActionResults, apply cost multiplier to actor's fatigue.
-// --> PlayerEndTurn       | checks if player lost MyTurn, resume process.
-
-
-#[derive(SystemData)]
-pub struct TimeSystemData<'a> {
-    actors: WriteStorage<'a, Actor>,
-    my_turns: WriteStorage<'a, MyTurn>,
-    game_state: WriteExpect<'a, crate::GameState>,
-}
-
-pub struct Time;
-impl<'a> System<'a> for Time {
-    type SystemData = TimeSystemData<'a>;
-    fn run(&mut self, mut data: Self::SystemData) {
-        if !data.game_state.player_turn {
-        }
-    }
-}
-=======
 use crate::components::*;
 use crate::components::flags::ActionResult;
 use crate::Turn;
->>>>>>> new-turn-system
 
 #[derive(SystemData)]
 pub struct TurnAllocatorSystemData<'a> {
     entities: Entities<'a>,
-<<<<<<< HEAD
-=======
     names: ReadStorage<'a, Name>,
->>>>>>> new-turn-system
     my_turns: WriteStorage<'a, MyTurn>,
     actors: WriteStorage<'a, Actor>,
     game_state: WriteExpect<'a, crate::GameState>,
     action_results: WriteStorage<'a, ActionResult>,
-<<<<<<< HEAD
-
-
-}
-
-pub struct TurnAllocator;
-=======
     turn_queue: WriteExpect<'a, crate::TurnQueue>,
 }
 
 pub struct TurnAllocator;
 
->>>>>>> new-turn-system
 impl<'a> System<'a> for TurnAllocator {
     type SystemData = TurnAllocatorSystemData<'a>;
     fn run(&mut self, mut data: Self::SystemData) {
         if !data.game_state.player_turn {
-<<<<<<< HEAD
-            let mut actors = (&data.actors, &data.entities, !&data.my_turns).join().collect::<Vec<_>>();
-            actors.sort_by(|&a, &b| a.0.next_turn.cmp(&b.0.next_turn));
-            
-            data.game_state.world_time.ticks = actors[0].0.next_turn;
-            let active_actors = actors.iter().filter(|&a| a.0.next_turn == actors[0].0.next_turn);
-            for active in active_actors {
-                let active_actor = active.0;
-                let active_entity = active.1;
-                data.my_turns.insert(active_entity, MyTurn{});
-                if active_actor.next_turn >= data.game_state.world_time.ticks {
-                    // println!("{:?}", active);
-                } else {
-                    panic!("Actor next_turn is in the past");
-                }
-=======
             for (actor, entity, name, _my_turn) in (&data.actors, &data.entities, &data.names, !&data.my_turns).join() {
                 let actor_next_turn = Turn {
                     tick: actor.next_turn,
@@ -103,7 +42,6 @@ impl<'a> System<'a> for TurnAllocator {
                 data.game_state.world_time.determine_world_turn();
                 data.my_turns.insert(turn.entity, MyTurn{});
                 // println!("turn queue length: {:?}", data.turn_queue.len());
->>>>>>> new-turn-system
             }
         }
     }
@@ -124,11 +62,8 @@ impl<'a> System<'a> for PlayerStartTurn {
         for (ent, _player) in (&data.entities, &data.players).join() {
             if let Some(_my_turn) = &data.my_turns.get(ent) {
                 data.game_state.player_turn = true;
-<<<<<<< HEAD
-=======
             } else {
                 data.game_state.player_turn = false;
->>>>>>> new-turn-system
             }
         }
     }
@@ -142,11 +77,8 @@ pub struct EndTurnSystemData<'a> {
     my_turns: WriteStorage<'a, MyTurn>,
     world_updater: Read<'a, LazyUpdate>,
     game_state: WriteExpect<'a, crate::GameState>,
-<<<<<<< HEAD
-=======
     turn_queue: WriteExpect<'a, crate::TurnQueue>,
     players: ReadStorage<'a, PlayerControl>,
->>>>>>> new-turn-system
 }
 
 pub struct EndTurn;
@@ -154,12 +86,6 @@ impl<'a> System<'a> for EndTurn {
     type SystemData = EndTurnSystemData<'a>;
     fn run(&mut self, mut data: Self::SystemData) {
         if !data.game_state.player_turn {
-<<<<<<< HEAD
-            for (ent, action_result, actor) in (&data.entities, &mut data.action_results, &mut data.actors).join() {
-                actor.set_next_turn_from_cost(data.game_state.world_time.ticks, action_result.cost);
-                data.world_updater.remove::<ActionResult>(ent);
-                // println!("{:?}", actor);
-=======
             for (ent, actor) in (&data.entities, &mut data.actors).join() {
                 if let Some(result) = data.action_results.get_mut(ent) {
                     actor.set_next_turn_from_cost(data.game_state.world_time.tick, result.cost);
@@ -169,7 +95,6 @@ impl<'a> System<'a> for EndTurn {
                     }
                     data.action_results.remove(ent);
                 }
->>>>>>> new-turn-system
             }
         }
     }
@@ -191,14 +116,6 @@ impl<'a> System<'a> for PlayerEndTurn {
     type SystemData = PlayerEndTurnSystemData<'a>;
     fn run(&mut self, mut data: Self::SystemData) {
         for (ent, player) in (&data.entities, &data.players).join() {
-<<<<<<< HEAD
-            if let Some(_) = data.my_turns.get(ent) {
-                return
-            }
-            data.game_state.player_turn = false;
-=======
-
->>>>>>> new-turn-system
         }
     }
 }
