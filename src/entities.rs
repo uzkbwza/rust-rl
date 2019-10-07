d8cac38173d04e849149bf8b3784265b9fe4ed1d
Ian Sly <iansly@protonmail.com>
2019-10-07 16:03:14 -0700

    added the shack back in

--- src/entities.rs
+++ src/entities.rs
@@ -111,7 +111,7 @@ pub fn create_test_map(world: &mut World) {
         create_wall(world, x, MAP_HEIGHT - 1);
     }
 
-//    create_shack(world, MAP_WIDTH/2, MAP_HEIGHT/2, 7);
+    create_shack(world, MAP_WIDTH/2, MAP_HEIGHT/2, 7);
 
      for _ in 0..50 {
          create_dummy(world);
